d26dd9268ecc67764deef866f153f3a76ebcf730
Khelde <94265348+muke78@users.noreply.github.com>
2025-12-07 22:49:55 -0600

    fix(response): Changing the viable response generates a new error in the UUID generator.

--- src/helpers/uuid-helper.js
+++ src/helpers/uuid-helper.js
@@ -1,10 +1,13 @@
 import crypto from "node:crypto";
-import { ValidationError } from "../utils/error-utils.js";
+import { InternalServerError } from "../utils/error-utils.js";
 
 export const generateUUID = () => {
 	try {
 		return crypto.randomUUID();
 	} catch (err) {
-		throw new ValidationError("Error interno al generar UUID.");
+		throw new InternalServerError(
+			"Error interno al generar UUID.",
+			err.message,
+		);
 	}
 };
