8cd513ccbcb2705f206dda7d0cd5292e925e87fc
Erick Muke <muke7881@gmail.com>
2026-01-11 05:28:00 -0600

    feat: Add formatting to reference number in the listRelationalDuplex Plugin model

--- src/models/archives/list-relational-duplex-models.js
+++ src/models/archives/list-relational-duplex-models.js
@@ -29,6 +29,7 @@ export const listRelationalDuplexPluginModel = async (
                         related_entries_id,
                         archive_id,
                         reference_number,
+						LPAD(reference_number, 2, '0') AS reference_formatted,
                         reference_folio,
                         description,
                         event_date,
